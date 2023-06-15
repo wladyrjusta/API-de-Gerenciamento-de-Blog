071e8b89101cc1c1d5641471d270a4891ce20b92
wladyrjusta <wladyr.justa@gmail.com>
2023-06-15 19:48:34 -0300

    [Wladyr Justa] - Project Blogs API

---
+++ src/migrations/20230615222608-create-users.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', { 
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      displayName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'display_name',
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: Sequelize.STRING,
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
